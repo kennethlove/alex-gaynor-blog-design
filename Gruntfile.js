cd7dd045c5189fb46417e9b5b5e35163a57b613a
Kenneth Love <kenneth@gigantuan.net>
2013-11-24 11:56:32 -0800

    stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,13 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     sass: {
-      options: {
-        includePaths: ['bower_components/foundation/scss']
-      },
       dist: {
         options: {
           outputStyle: 'compressed'
         },
         files: {
           'css/app.css': 'scss/app.scss'
-        }        
+        }
       }
     },
 
